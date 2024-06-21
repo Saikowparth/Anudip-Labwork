d112f2c91ebd93b9efe6ccc3d30a92c69c33c55f
Parth Nakti <103308687+Saikowparth@users.noreply.github.com>
2024-06-21 21:40:51 +0530

    Add files via upload

---
+++ ArrayList/Loan.java
@@ -0,0 +1,10 @@
+package com.Anudip;
+
+public interface Loan {
+        // Method to calculate loan amount
+        double loanAmount();
+
+        // Method to accept loan details
+        void accept();
+    }
+
